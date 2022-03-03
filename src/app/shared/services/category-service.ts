41e9ed6d1b6f7a9e08e93bfba58052ee4a9204ef
MahmoudAbdElrdy <47526228+MahmoudAbdElrdy@users.noreply.github.com>
2022-03-03 22:22:33 +0200

    site

--- src/app/shared/services/category-service.ts
+++ src/app/shared/services/category-service.ts
@@ -13,33 +13,33 @@ export class CategoryService {
   constructor(private _httpClient: HttpClient) { }
  
 
-  createUpdate(Category: any):Observable<ResponseData>{
-    return this._httpClient.post<any>(environment.apiUrl + "/Category/CreateUpdate", Category);
+  createUpdate(CategorySite: any):Observable<ResponseData>{
+    return this._httpClient.post<any>(environment.apiUrl + "/CategorySite/CreateUpdate", CategorySite);
   } 
 
   getAll(filter):Observable<ResponseData>{
-    return this._httpClient.post<any>(environment.apiUrl + "/Category/GetAll",filter);
+    return this._httpClient.post<any>(environment.apiUrl + "/CategorySite/GetAll",filter);
   } 
 
   getById(id:any):Observable<any>{
     let params = new HttpParams();
     params = params.append('id', id);
-    return this._httpClient.get<any>(environment.apiUrl + "/Category/GetById", { params: params});
+    return this._httpClient.get<any>(environment.apiUrl + "/CategorySite/GetById", { params: params});
   } 
 
   delete(id: any):Observable<any>{
     let params = new HttpParams();
     params = params.append('id', id);
-    return this._httpClient.get<any>(environment.apiUrl + "/Category/Delete", { params: params});
+    return this._httpClient.get<any>(environment.apiUrl + "/CategorySite/Delete", { params: params});
   } 
 
   getAllByCategoryId(filter:any):Observable<any>{
-    return this._httpClient.post<any>(environment.apiUrl + "/Category/GetAllByCategoryId",filter);
+    return this._httpClient.post<any>(environment.apiUrl + "/CategorySite/GetAllByCategoryId",filter);
   }
   getAllBar(filter):Observable<ResponseData>{
-    return this._httpClient.post<any>(environment.apiUrl + "/Category/GetAllBar",filter);
+    return this._httpClient.post<any>(environment.apiUrl + "/CategorySite/GetAllBar",filter);
   } 
   getAllChoose(filter):Observable<ResponseData>{
-    return this._httpClient.post<any>(environment.apiUrl + "/Category/GetAllChoose",filter);
+    return this._httpClient.post<any>(environment.apiUrl + "/CategorySite/GetAllChoose",filter);
   } 
 }
