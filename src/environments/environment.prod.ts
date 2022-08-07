738f50f58defdaf34e39c60f637ccbccf675c727
MahmoudAbdElrdy <47526228+MahmoudAbdElrdy@users.noreply.github.com>
2022-08-07 03:13:17 +0200

    concat

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl:'http://mahmoudabd2345-002-site1.ctempurl.com/api',
- url:'http://mahmoudabd2345-002-site1.ctempurl.com'
+//   apiUrl:'http://mahmoudabd2345-002-site1.ctempurl.com/api',
+//  url:'http://mahmoudabd2345-002-site1.ctempurl.com'
+url:'http://islamragheb-001-site2.itempurl.com',
+apiUrl:'http://islamragheb-001-site2.itempurl.com/api',
 };
